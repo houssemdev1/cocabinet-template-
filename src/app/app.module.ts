6c1e67fb4b84d59b2f626a8ca1d8cefd0196ea53
houssemdev1 <houssem.najeh@outlook.com>
2019-03-19 17:24:47 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { FooterPageComponent } from './footer-page/footer-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    ContentComponent,
+    FooterComponent,
+    TestimonyComponent,
+    FooterPageComponent
   ],
   imports: [
     BrowserModule,
